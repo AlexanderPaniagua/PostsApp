bcd998aa4403e1299f225cc22f33c2e4b45090d5
AlexanderPaniagua <alexanderpaniiagua@gmail.com>
2022-02-06 11:49:25 -0600

    Initial Commit

---
+++ PostsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PostsApp
+//
+//  Created by Alexander Paniagua on 6/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
